b35582f43c40501dd8520233e89d1185f9ff51af
devruji <devruji>
2024-08-15 10:13:55 +0000

    initial commit

---
+++ includes/configuration.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+raw_folder_path: str = "/mnt/bossrujiformula1dl/raw"
+processed_folder_path: str = "/mnt/bossrujiformula1dl/processed"
+presentation_folder_path: str = "/mnt/bossrujiformula1dl/presentation"
